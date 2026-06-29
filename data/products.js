3fe90ee7a030968be4b226a8f6b74d0fefdadaed
nathan mubvuta <135868806+nathanMub@users.noreply.github.com>
2026-06-29 08:42:19 +0200

    Add products data to products.js

---
+++ data/products.js
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: "after-school-dream",
+    name: "After School Dream",
+    price: 29.99,
+    size: ["M", "L", "XXL"],
+    image: "",
+    story:
+      "A glowing memory of walking home after school when everything felt infinite.",
+  },
+  {
+    id: "cartoon-static",
+    name: "Cartoon Static",
+    price: 24.99,
+    size: ["M", "L"],
+    image: "",
+    story:
+      "Late-night cartoons playing through soft TV static and imagination.",
+  },
+  {
+    id: "galaxy-playground",
+    name: "Galaxy Playground",
+    price: 34.99,
+    size: ["L", "XXL"],
+    image: "",
+    story:
+      "A playground floating through space where every swing hits a star.",
+  },
+];
